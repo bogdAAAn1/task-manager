427eec9f53ea0ed617284c7e0d1cb05dddd79ec7
bogdAAAn1 <privetpokaxaxaxa@gmail.com>
2025-02-03 17:38:48 +0200

    Add db.sqlite3 to .gitignore

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -123,4 +123,4 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-AUTH_USER_MODEL = "users.Worker"
+AUTH_USER_MODEL = "tasks.Worker"
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,50 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Position(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(
+        Position,
+        related_name="workers",
+        on_delete=models.SET_NULL,
+        null=True
+    )
+
+
+class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ("High", "High"),
+        ("Urgent", "Urgent"),
+        ("Medium", "Medium"),
+        ("Low", "Low"),
+    ]
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(
+        max_length=6,
+        choices=PRIORITY_CHOICES,
+        default="Low"
+    )
+    task_type = models.ForeignKey(
+        TaskType,
+        related_name="tasks",
+        on_delete=models.CASCADE
+    )
+    assignees = models.ManyToManyField(Worker, related_name="assigned_tasks")
