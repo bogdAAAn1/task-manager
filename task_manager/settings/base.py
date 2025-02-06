1155546fd8ae8bee1d2cdf6d08a0b40e2bfd8bc5
bogdAAAn1 <privetpokaxaxaxa@gmail.com>
2025-02-06 11:34:18 +0200

    remove STATIC_ROOT and fir STATICFILES_DIRS

--- task_manager/settings/base.py
+++ task_manager/settings/base.py
@@ -114,9 +114,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-STATICFILES_DIRS = [BASE_DIR / "static"]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
