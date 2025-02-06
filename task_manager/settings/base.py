0d4d1380f8874937c4505168cfb5d447b5534227
bogdAAAn1 <privetpokaxaxaxa@gmail.com>
2025-02-06 17:33:36 +0200

    fix base.py

--- task_manager/settings/base.py
+++ task_manager/settings/base.py
@@ -107,10 +107,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = "staticfiles/"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
-# STATICFILES_DIRS = [BASE_DIR, "static"]
+STATICFILES_DIRS = (BASE_DIR / "static",)
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
